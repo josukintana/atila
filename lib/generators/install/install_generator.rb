0b6a7c8d66d34ea3c666b8d89dd3daa4581ebb60
Josue Quintana <josukintana@gmail.com>
2012-07-02 22:35:24 +0100

    Changed conditions in order to generate all modules, being able to skip every module. This allows the user to skip those modules already generated.

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -5,9 +5,13 @@ module Atila
 	module Generators
 		class InstallGenerator < Rails::Generators::Base
 		  include Rails::Generators::Migration
+
 		  source_root File.expand_path('../templates', __FILE__)
+		  class_option :with_all, :type => :boolean, :default => false, :description => "Install all modules."
 		  class_option :with_friendships, :type => :boolean, :default => false, :description => "Install friendships management module."
+		  class_option :skip_friendships, :type => :boolean, :default => false, :description => "Skip friendships management module."
 		  class_option :with_groups, :type => :boolean, :default => false, :description => "Install groups management module."
+		  class_option :skip_groups, :type => :boolean, :default => false, :description => "Skip groups management module."
 
 		  	# def set_application
 		  	# 	environment "config.autoload_paths += %W(#{config.root}/lib)"
@@ -16,20 +20,31 @@ module Atila
 
 		  	def install
 		  		#Copy models
-		        template "models/friendship.rb", "app/models/friendship.rb" if options.with_friendships?
-		        template "models/group.rb", "app/models/group.rb" if options.with_groups?
-		        template "models/membership.rb", "app/models/membership.rb" if options.with_groups?
-		        template "models/ownership.rb", "app/models/ownership.rb" if options.with_groups?
+		        if options.with_friendships? || (options.with_all? && !options.skip_friendships)
+		        	#Copy models
+		        	template "models/friendship.rb", "app/models/friendship.rb"
 
-		        #Copy modules
-		        template "modules/friendships_management.rb", "lib/atila/friendships_management.rb" if options.with_friendships?
-		        template "modules/groups_management.rb", "lib/atila/groups_management.rb" if options.with_groups?
+		        	#Copy modules
+		        	template "modules/friendships_management.rb", "lib/atila/friendships_management.rb"
 
-		        #Generate migrations
-		        migration_template 'migrations/create_friendships.rb', 'db/migrate/create_friendships.rb' if options.with_friendships?
-		        migration_template 'migrations/create_groups.rb', 'db/migrate/create_groups.rb' if options.with_groups?
-		        migration_template 'migrations/create_memberships.rb', 'db/migrate/create_memberships.rb' if options.with_groups?
-		        migration_template 'migrations/create_ownerships.rb', 'db/migrate/create_ownerships.rb' if options.with_groups?
+		        	#Generate migrations
+		        	migration_template "migrations/create_friendships.rb", "db/migrate/create_friendships.rb"
+		        end
+		        
+		        if options.with_groups? || (options.with_all? && !options.skip_groups)
+		        	#Copy models
+			        template "models/group.rb", "app/models/group.rb"
+			        template "models/membership.rb", "app/models/membership.rb"
+			        template "models/ownership.rb", "app/models/ownership.rb"
+
+			        #Copy modules
+			        template "modules/groups_management.rb", "lib/atila/groups_management.rb"
+
+			        #Generate migrations
+			        migration_template "migrations/create_groups.rb", "db/migrate/create_groups.rb" 
+		        	migration_template "migrations/create_memberships.rb", "db/migrate/create_memberships.rb"
+		        	migration_template "migrations/create_ownerships.rb", "db/migrate/create_ownerships.rb"
+		    	end
 		  	end
 
 		  	def self.next_migration_number(path)
