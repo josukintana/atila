604df26ca48e8a9dd70364c68b36a733645a09c8
Josue Quintana <josukintana@gmail.com>
2012-07-06 20:22:28 +0100

    Made 'followed_id' attribute accessible.

--- lib/generators/install/templates/models/followment.rb
+++ lib/generators/install/templates/models/followment.rb
@@ -1,4 +1,6 @@
 class Followment < ActiveRecord::Base
   belongs_to :follower, :foreign_key => "user_id", :class_name => 'Followment'
   belongs_to :followed, :class_name => 'User'
+
+  attr_accessible :followed_id
 end
