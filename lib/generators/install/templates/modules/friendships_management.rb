7ce687458b88935ab9573eea62528c5dbd3e8479
Josue Quintana <josukintana@gmail.com>
2012-07-15 12:09:32 +0100

    Set RDoc comments.

--- lib/generators/install/templates/modules/friendships_management.rb
+++ lib/generators/install/templates/modules/friendships_management.rb
@@ -14,26 +14,36 @@ module FriendshipsManagement
   class UserCannotBeRequested < StandardError; end
   
   module InstanceMethods
+
+    # Gets the users that are friends of the calling user.
+    # A user's non_followers are all the users except the calling user and his followers.
     def non_friends
       User.where("id != ?", self.id) - self.friends #My non_friends are all the users but me and my friends
     end
     
+    # Determines if a user is already a friend of another user.
     def is_friend_of?(friend)
-      # Determines if a user is already a friend of mine
       self.friends.include?(friend)
     end
     
+    # Determines if a user is blocked by another user.
     def is_blocked_by?(friend)
       friend.blocked_friends.include?(self)
     end
     
-    def can_request_to?(friend) #Friendships can be requested if users are not already friends and they have not got a pending friendship
+    # Determines if a user can request friendship to another user.
+    # Friendships can be requested if users are not already friends and they have not got a pending friendship.
+    def can_request_to?(friend)
       ((!self.is_friend_of?(friend)) && (!self.pending_friends.include?(friend)) && (!self.requested_friends.include?(friend)))
     end
     
+    # Requests a user to be friend of the calling user.
     def request_friendship_to(friend)
-      unless (!self.can_request_to?(friend))     #Only someone who is not still my friend can be requested
-        unless self.is_blocked_by?(friend)  #If the user is blocked by its potential friend, the friend will
+      unless (!self.can_request_to?(friend))     
+        #--
+        # If the user is blocked by its potential friend, the friend will not be requested.
+        #++
+        unless self.is_blocked_by?(friend)
           friend.friendships.create(:friend_id => self.id, :status => Friendship::PENDING)  
         end
         
@@ -46,16 +56,18 @@ module FriendshipsManagement
         raise UserCannotBeRequested, self.email + " cannot request " + friend.email + "'s friendship."
       end
     end
-    
+
+    # Cancels a request of friendship from the calling user to another user.
     def cancel_request_to(friend)
       unless (!self.requested_friends.include?(friend))
-        self.friendships.by_friend(friend).requested.first.destroy #Cancel my friendship request.
-        friend.friendships.by_friend(self).pending.first.destroy #Cancel my friend's pending request.
+        self.friendships.by_friend(friend).requested.first.destroy
+        friend.friendships.by_friend(self).pending.first.destroy
       else
         raise NoRequestedFriendship, "There is no friendship requested by " + user.email + " to " + friend.email + "."
       end
     end
     
+    # Accepts a friendship requested by another user to the calling user.
     def accept_friendship_of(friend)
       unless (!self.pending_friends.include?(friend))
         self.friendships.by_friend(friend).pending.first.accept #I'm the friend of my friend.
@@ -65,6 +77,7 @@ module FriendshipsManagement
       end
     end
     
+    # Rejects a friendship requested by another user to the calling user.
     def reject_friendship_of(friend)
       unless (!self.pending_friends.include?(friend))
         self.friendships.by_friend(friend).pending.first.destroy #My friendship is destroyed.
@@ -74,6 +87,7 @@ module FriendshipsManagement
       end
     end
     
+    # Cancels an existing friendship between the calling user and another user.
     def cancel_friendship_with(friend)
       unless (!self.is_friend_of?(friend))
         self.friendships.by_friend(friend).accepted.first.destroy #My friendship is destroyed.
@@ -83,6 +97,7 @@ module FriendshipsManagement
       end
     end
     
+    # Blocks another user to avoid him to request a friendship to the calling user.
     def block_friend(friend)
       if ((self.can_request_to?(friend)) && (!self.blocked_friends.include?(friend)))
         self.friendships.create(:friend_id => friend.id, :status => Friendship::BLOCKED) #My friendship is blocked.
@@ -92,6 +107,7 @@ module FriendshipsManagement
       end
     end
     
+    # Unblock a previously blocked user.
     def unblock_friend(friend)
       unless (!self.blocked_friends.include?(friend))
         self.friendships.by_friend(friend).first.destroy #The blocked friendship is destroyed.
@@ -102,6 +118,7 @@ module FriendshipsManagement
   end
  
   module ClassMethods
+    # Sets up the necessary relationships between the User model and the Friendship model. 
     def initialize
       has_many :friendships, :dependent => :destroy
       has_many :friends, :through => :friendships, :conditions => "status = 'accepted'"
