7a31743dd165729d4cd31e736944628e74c48e9a
Josue Quintana <josukintana@gmail.com>
2012-07-15 12:15:04 +0100

    Set RDoc comments.

--- lib/generators/install/templates/modules/groups_management.rb
+++ lib/generators/install/templates/modules/groups_management.rb
@@ -10,10 +10,13 @@ module GroupsManagement
   class GroupNotFound < StandardError; end
 
   module InstanceMethods
+
+    # Determines if a group with the given name already exists.
     def group_exists?(name)
       !self.groups.find_by_name(name).nil?
     end
     
+    # Creates a new group with the given where the calling user is the owner.
     def add_group(name)
       unless group_exists?(name)
         self.groups.create(:name => name)
@@ -22,17 +25,19 @@ module GroupsManagement
       end
     end
     
+    # Deletes a group with the given name where the calling user is its owner.
     def remove_group(name)
       unless !group_exists?(name)
         self.groups.find_by_name(name).destroy
       else
-        raise GroupNotFound, "The group '" + name + "' doesn't exist."
+        raise GroupNotFound, "The group '" + name + "' does not exist."
       end
     end
     
   end
  
   module ClassMethods
+    # Sets up the necessary relationships between the User model and the Membership and Ownership models. 
     def initialize
       has_many :memberships
       has_many :ownerships, :dependent => :destroy
