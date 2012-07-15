42152d59b5a486b1bf49c12c8e4044268f62e594
Josue Quintana <josukintana@gmail.com>
2012-07-15 12:16:44 +0100

    Set RDoc comments.

--- lib/generators/install/templates/modules/profiles_management.rb
+++ lib/generators/install/templates/modules/profiles_management.rb
@@ -6,12 +6,14 @@ module ProfilesManagement
   end
  
   module InstanceMethods
+    # Creates a profile for the specified user.
     def create_profile
       Profile.create(:user_id => self.id)
     end
   end
  
   module ClassMethods
+    # Sets up the necessary relationships between the User model and the Profile model. 
     def initialize
       has_one :profile
       
